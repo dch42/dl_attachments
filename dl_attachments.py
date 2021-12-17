9ec8c63f8f4915b26c9cef5bfbf81b15b6a2598d
ddch42 <85430824+dch42@users.noreply.github.com>
2021-12-16 21:07:03 -0600

    build query string

--- dl_attachments.py
+++ dl_attachments.py
@@ -81,48 +81,53 @@ def set_mailbox(imap_ssl):
                 imap_ssl.select(
                     mailbox=f'{ROOT_INBOX}/{arg}', readonly=RO)
                 search_mail(imap_ssl)
-
     else:
         imap_ssl.select(mailbox=f'{ROOT_INBOX}', readonly=RO)
         search_mail(imap_ssl)
 
+
+def create_query(term=False, address=False):
+    """Build query string for mailbox"""
+    query = '%s%s%s' % ("UNSEEN " if args.unseen else '', f'FROM "{address}" ' if args.address else '', f'SUBJECT "{term}" ' if args.terms else '')
+    query = query.rstrip()
+    return query
+
+
+def get_mail_list(imap_ssl, query):
+    """Return list of mails matching query"""
+    mail = imap_ssl.search(None, f'({query})')[1]
+    return mail
+    
 
 def search_mail(imap_ssl):
     """Set email search params"""
     if args.terms and args.address:
         for address in args.address:
             for term in args.terms:
-                if args.unseen:
-                    resp_code, mails = imap_ssl.search(
-                        None, f'(UNSEEN FROM "{address}" SUBJECT "{term}")')
-                    list_mails(imap_ssl, mails)
-                else:
-                    resp_code, mails = imap_ssl.search(
-                        None, f'(FROM "{address}" SUBJECT "{term}")')
-                    list_mails(imap_ssl, mails)
+                query = create_query(term=term, address=address)
+                mail = get_mail_list(imap_ssl, query)
+                list_mail(imap_ssl, mail)
     elif args.terms and not args.address:
         for term in args.terms:
-            resp_code, mails = imap_ssl.search(None, f'(SUBJECT "{term}")')
-            list_mails(imap_ssl, mails)
+            query = create_query(term=term)
+            mail = get_mail_list(imap_ssl, query)
+            list_mail(imap_ssl, mail)
     elif args.address and not args.terms:
         for address in args.address:
-            if args.unseen:
-                resp_code, mails = imap_ssl.search(
-                    None, f'(UNSEEN FROM "{address}")')
-                list_mails(imap_ssl, mails)
-            else:
-                resp_code, mails = imap_ssl.search(None, f'(FROM "{address}")')
-                list_mails(imap_ssl, mails)
+            query = create_query(term=False, address=address)
+            mail = get_mail_list(imap_ssl, query)
+            list_mail(imap_ssl, mail)
     else:
         if args.unseen:
-            resp_code, mails = imap_ssl.search(None, "UNSEEN")
-            list_mails(imap_ssl, mails)
+            query = create_query(term=False, address=False)
+            mail = get_mail_list(imap_ssl, query)
+            list_mail(imap_ssl, mail)
         else:
-            resp_code, mails = imap_ssl.search(None, "ALL")
-            list_mails(imap_ssl, mails)
+            mails = imap_ssl.search(None, "ALL")[1]
+            list_mail(imap_ssl, mails)
 
 
-def list_mails(imap_ssl, mails):
+def list_mail(imap_ssl, mails):
     """Loop through messages"""
     mail_ids = mails[0].decode().split()
     print(f"{len(mail_ids)} emails found...\n")
