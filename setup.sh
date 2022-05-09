a526fa6a1e9ace1ae93a7af4a239e1ed513c2c9b
ddch42 <85430824+dch42@users.noreply.github.com>
2022-05-08 23:37:50 -0700

    add linux, fix requirements and cfg

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 declare script_name="dla"
-declare personal_bin="/Users/$USER/bin"
-declare hidden_dir="/Users/$USER/.$script_name"
+declare ext="py"
 declare cfg=".bash_profile"
 
 declare grn="\e[0;92m"
@@ -16,42 +15,38 @@ function make_dir {
 
 function add_to_path {
     printf "Adding '$personal_bin' to \$PATH in $cfg...\n" & 
-    echo export PATH="$personal_bin:\$PATH" >> /Users/$USER/$cfg
+    echo export PATH="$personal_bin:\$PATH" >> $home/$cfg
 }
-
- # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-###############################################################
- # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 printf "\nAttempting installation of script '$script_name'...\n\n"
 
-echo "Installing requirements..."
+[[ -f './requirements.txt' ]] && 
+echo "Installing requirements..." &&
 eval pip3 install -r requirements.txt
+
+case $OSTYPE in 
+    "darwin"*) declare home="/Users/$USER" ;;
+    "linux"*) declare home="/home/$USER" ;;
+esac
+
+declare personal_bin="$home/bin"
+declare hidden_dir="$home/.$script_name"
 
 [ -n $ZSH_VERSION ] && 
 cfg=".zprofile"
 
 [[ ! -d $personal_bin ]] && 
 make_dir ||
-printf "'$personal_bin' exists...\n"
+printf "\n${grn}[OK]${reset} '$personal_bin' exists...\n"
 
-grep -q "$personal_bin" /Users/$USER/$cfg && 
-printf "'$personal_bin' already in \$PATH...\n" ||
+grep -q "$personal_bin" $home/$cfg && 
+printf "${grn}[OK]${reset} '$personal_bin' already in \$PATH...\n" ||
 add_to_path
 
-echo "Installing $script_name..."
-chmod +x ./${script_name}.py &&
-cp ./${script_name}.py $personal_bin/$script_name &&
-printf "${grn}[SUCCESS]${reset} Script $script_name installed at '$personal_bin/$script'!\n\n" ||
-printf "${red}[ERROR]${reset} Something went wrong...\n" exit 1
+printf "\nInstalling '$script_name'...\n"
+chmod +x ./${script_name}.$ext &&
+cp ./${script_name}.$ext $personal_bin/$script_name &&
+printf "\n${grn}[SUCCESS]${reset} Script '$script_name' installed at '$personal_bin/$script_name'!\n\n" ||
+printf "\n${red}[ERROR]${reset} Something went wrong...\n" exit 1
 
-[[ ! -d $hidden_dir ]] &&
-mkdir -pv $hidden_dir
-
-read -p "Config file will now open, please hit 'ENTER' and fill in the required info..."
-nano ./config.yml &&
-cp ./config.yml  ${hidden_dir}/${script_name}_config.yml &&
-cp ./cfg_crypt.py $personal_bin &&
-printf "Config file moved to ${hidden_dir}/${script_name}_config.yml...\n"
-printf "${grn}[SUCCESS]${reset} For help, invoke '$script_name -h'\n"
 exit 0
